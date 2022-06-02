6f11b81e4eb7d9d24620a82fc003c6c24335030e
WongJapan <kulomilyarder@gmail.com>
2022-06-02 12:18:16 +0700

    Update njord.js
    checkpoint test,
    fixed test : Deployment, Distribution, Transactions at Transfer is Disabled State

--- test/njord.js
+++ test/njord.js
@@ -2,6 +2,8 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 // const dotenv = require("dotenv");
 
+const { zeroAddress } = require("./helpers/address");
+
 const {
   NJORD_TOTAL_SUPPLY,
   INITIAL_NJORD_LIQUIDITY,
@@ -224,79 +226,364 @@ describe("Njord Token", function () {
     });
   });
 
-  describe("Only Owner Write Methods (Only TreasuryFund Address can use them)", function () {
-    before(async function () {
-      // Deploy contract
-      const Token = await ethers.getContractFactory("NjordContract");
-      token = await Token.deploy(
-        pancakeRouterContract.address,
-        autoLiquidityFund.address,
-        treasuryFund.address,
-        njordRiskFreeFund.address,
-        supplyControl.address,
-      );
-      await token.deployed();
+  describe("Funtion Test", function () {
+    describe("setRebaseRate Function Test", function () {
+      before(async function () {
+        // Deploy contract
+        const Token = await ethers.getContractFactory("NjordContract");
+        token = await Token.deploy(
+          pancakeRouterContract.address,
+          autoLiquidityFund.address,
+          treasuryFund.address,
+          njordRiskFreeFund.address,
+          supplyControl.address,
+        );
+        await token.deployed();
+      });
+
+      it("Only Onwer can call this function", async function () {
+        await expect(
+          token.connect(client1).setRebaseRate(2000),
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+        await expect(token.connect(treasuryFund).setAutoRebase(2000)).to.not.be
+          .reverted;
+      });
+
+      it("Should Can't set value, if new value is same with old value", async function () {
+        // set to 2000 first
+        await token.connect(treasuryFund).setRebaseRate(2000);
+        // so if we set 2000 again, its will revert
+        await expect(
+          token.connect(treasuryFund).setRebaseRate(2000),
+        ).to.be.revertedWith("Nothing Changed");
+      });
+
+      it("Should set correct value and emit event LogRebaseRateChanged", async function () {
+        const rebaseRateSet = 2475;
+        const oldRebaseRate = await token.ownerRebaseRate();
+
+        /**
+         * event fired :
+         * LogRebaseRateChanged(oldRate, newRate)
+         *
+         */
+        expect(await token.connect(treasuryFund).setRebaseRate(rebaseRateSet))
+          .to.emit(token, "LogRebaseRateChanged")
+          .withArgs(oldRebaseRate, rebaseRateSet);
+
+        const newRebaseRate = await token.ownerRebaseRate();
+        expect(newRebaseRate).to.eq(rebaseRateSet);
+      });
     });
 
-    it("Set Auto Rebase", async function () {
-      await expect(
-        token.connect(client1).setAutoRebase(true),
-      ).to.be.revertedWith("Ownable: caller is not the owner");
-      await expect(token.connect(treasuryFund).setAutoRebase(true)).to.emit(
-        token,
-        "LogAutoRebaseChanged",
-      );
+    describe("toggleOwnerRebase Function Test", function () {
+      before(async function () {
+        // Deploy contract
+        const Token = await ethers.getContractFactory("NjordContract");
+        token = await Token.deploy(
+          pancakeRouterContract.address,
+          autoLiquidityFund.address,
+          treasuryFund.address,
+          njordRiskFreeFund.address,
+          supplyControl.address,
+        );
+        await token.deployed();
+      });
+
+      it("Only Onwer can call this function", async function () {
+        await expect(
+          token.connect(client1).toggleOwnerRebase(),
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+        await expect(token.connect(treasuryFund).toggleOwnerRebase()).to.not.be
+          .reverted;
+      });
+
+      it("Should set correct value and emit event LogRebaseOwnerChanged", async function () {
+        const oldStatus = await token.isOwnerRebaseEnabled();
+        /**
+         * event fired :
+         * LogRebaseOwnerChanged(oldStatus, newStatus)
+         *
+         */
+        expect(await token.connect(treasuryFund).toggleOwnerRebase())
+          .to.emit(token, "LogRebaseOwnerChanged")
+          .withArgs(oldStatus, !oldStatus);
+
+        const newStatus = await token.isOwnerRebaseEnabled();
+        expect(newStatus).to.eq(!oldStatus);
+      });
     });
 
-    it("Toggle Owner Rebase", async function () {
-      const initialOwnerRebaseState = await token.isOwnerRebaseEnabled();
-      await expect(
-        token.connect(client1).toggleOwnerRebase(),
-      ).to.be.revertedWith("Ownable: caller is not the owner");
+    describe("toggleTransferStatus Function Test", function () {
+      before(async function () {
+        // Deploy contract
+        const Token = await ethers.getContractFactory("NjordContract");
+        token = await Token.deploy(
+          pancakeRouterContract.address,
+          autoLiquidityFund.address,
+          treasuryFund.address,
+          njordRiskFreeFund.address,
+          supplyControl.address,
+        );
+        await token.deployed();
+      });
 
-      expect(await token.connect(treasuryFund).toggleOwnerRebase()).to.emit(
-        token,
-        "LogRebaseRateOwnerChanged",
-      );
+      it("Only Onwer can call this function", async function () {
+        await expect(
+          token.connect(client1).toggleTransferStatus(),
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+        await expect(token.connect(treasuryFund).toggleTransferStatus()).to.not
+          .be.reverted;
+      });
+
+      it("Should set correct value and emit event LogTransferStatusChanged", async function () {
+        const oldStatus = await token.isTransferEnabled();
+        /**
+         * event fired :
+         * LogTransferStatusChanged(oldStatus, newStatus)
+         *
+         */
+        expect(await token.connect(treasuryFund).toggleTransferStatus())
+          .to.emit(token, "LogTransferStatusChanged")
+          .withArgs(oldStatus, !oldStatus);
+
+        const newStatus = await token.isTransferEnabled();
+        expect(newStatus).to.eq(!oldStatus);
+      });
     });
 
-    it("Toggle Transfer State", async function () {
-      await expect(
-        token.connect(client1).toggleTransferStatus(),
-      ).to.be.revertedWith("Ownable: caller is not the owner");
+    describe("toggleTradingStatus Function Test", function () {
+      before(async function () {
+        // Deploy contract
+        const Token = await ethers.getContractFactory("NjordContract");
+        token = await Token.deploy(
+          pancakeRouterContract.address,
+          autoLiquidityFund.address,
+          treasuryFund.address,
+          njordRiskFreeFund.address,
+          supplyControl.address,
+        );
+        await token.deployed();
+      });
 
-      expect(await token.connect(treasuryFund).toggleTransferStatus()).to.emit(
-        token,
-        "LogTransferStatusChanged",
-      );
+      it("Only Onwer can call this function", async function () {
+        await expect(
+          token.connect(client1).toggleTradingStatus(),
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+        await expect(token.connect(treasuryFund).toggleTradingStatus()).to.not
+          .be.reverted;
+      });
+
+      it("Should set correct value and emit event LogTradingStatusChanged", async function () {
+        const oldStatus = await token.isTradingEnabled();
+        /**
+         * event fired :
+         * LogTradingStatusChanged(oldStatus, newStatus)
+         *
+         */
+        expect(await token.connect(treasuryFund).toggleTradingStatus())
+          .to.emit(token, "LogTradingStatusChanged")
+          .withArgs(oldStatus, !oldStatus);
+
+        const newStatus = await token.isTradingEnabled();
+        expect(newStatus).to.eq(!oldStatus);
+      });
     });
 
-    it("Toggle Trading State", async function () {
-      await expect(
-        token.connect(client1).toggleTradingStatus(),
-      ).to.be.revertedWith("Ownable: caller is not the owner");
+    describe("setAutoRebase Function Test", function () {
+      before(async function () {
+        // Deploy contract
+        const Token = await ethers.getContractFactory("NjordContract");
+        token = await Token.deploy(
+          pancakeRouterContract.address,
+          autoLiquidityFund.address,
+          treasuryFund.address,
+          njordRiskFreeFund.address,
+          supplyControl.address,
+        );
+        await token.deployed();
+      });
 
-      expect().to.emit(token, "LogTradingStatusChanged");
+      it("Only Onwer can call this function", async function () {
+        await expect(
+          token.connect(client1).setAutoRebase(true),
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+        await expect(token.connect(treasuryFund).setAutoRebase(true)).to.not.be
+          .reverted;
+      });
+
+      it("Should Can't set value, if new value is same with old value", async function () {
+        const oldStatus = await token._autoRebase();
+        // so if we set same value again, its will revert
+        await expect(
+          token.connect(treasuryFund).setAutoRebase(oldStatus),
+        ).to.be.revertedWith("Nothing Changed");
+      });
+
+      it("Should set correct value and emit event LogAutoRebaseChanged", async function () {
+        const oldStatus = await token._autoRebase();
+        const newStatus = !oldStatus;
+        /**
+         * event fired :
+         * LogAutoRebaseChanged(oldRate, newRate)
+         *
+         */
+        expect(await token.connect(treasuryFund).setAutoRebase(newStatus))
+          .to.emit(token, "LogAutoRebaseChanged")
+          .withArgs(oldStatus, newStatus);
+
+        const newRebaseStatus = await token._autoRebase();
+        expect(newRebaseStatus).to.eq(newStatus);
+      });
     });
 
-    it("Add Whitelisted Address", async function () {
-      expect(
-        token.connect(client1).setWhitelist(client5.address),
-      ).to.be.revertedWith("Ownable: caller is not the owner");
+    describe("setAutoAddLiquidity Function Test", function () {
+      before(async function () {
+        // Deploy contract
+        const Token = await ethers.getContractFactory("NjordContract");
+        token = await Token.deploy(
+          pancakeRouterContract.address,
+          autoLiquidityFund.address,
+          treasuryFund.address,
+          njordRiskFreeFund.address,
+          supplyControl.address,
+        );
+        await token.deployed();
+      });
 
-      expect(
-        await token.connect(treasuryFund).setWhitelist(client5.address),
-      ).to.emit(token, "LogWhitelistAdded");
+      it("Only Onwer can call this function", async function () {
+        await expect(
+          token.connect(client1).setAutoAddLiquidity(true),
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+
+        // because if we set same value, it will be reverted
+        const oldStatus = await token._autoAddLiquidity();
+        await expect(
+          token.connect(treasuryFund).setAutoAddLiquidity(!oldStatus),
+        ).to.not.be.reverted;
+      });
+
+      it("Should Can't set value, if new value is same with old value", async function () {
+        const oldStatus = await token._autoAddLiquidity();
+        // so if we set same value again, its will revert
+        await expect(
+          token.connect(treasuryFund).setAutoAddLiquidity(oldStatus),
+        ).to.be.revertedWith("Nothing Changed");
+      });
+
+      it("Should set correct value and emit event LogAutoLiquidityChanged", async function () {
+        const oldStatus = await token._autoAddLiquidity();
+        const newStatus = !oldStatus;
+        /**
+         * event fired :
+         * LogAutoLiquidityChanged(oldRate, newRate)
+         *
+         */
+        expect(await token.connect(treasuryFund).setAutoAddLiquidity(newStatus))
+          .to.emit(token, "LogAutoLiquidityChanged")
+          .withArgs(oldStatus, newStatus);
+
+        const newRebaseStatus = await token._autoAddLiquidity();
+        expect(newRebaseStatus).to.eq(newStatus);
+      });
     });
 
-    it("Remove Whitelisted Address", async function () {
-      expect(
-        token.connect(client1).removeWhitelist(client5.address),
-      ).to.be.revertedWith("Ownable: caller is not the owner");
+    describe("setWhitelist Function Test", function () {
+      before(async function () {
+        // Deploy contract
+        const Token = await ethers.getContractFactory("NjordContract");
+        token = await Token.deploy(
+          pancakeRouterContract.address,
+          autoLiquidityFund.address,
+          treasuryFund.address,
+          njordRiskFreeFund.address,
+          supplyControl.address,
+        );
+        await token.deployed();
+      });
 
-      expect(
-        await token.connect(treasuryFund).removeWhitelist(client5.address),
-      ).to.emit(token, "LogWhitelistRemoved");
+      it("Only Onwer can call this function", async function () {
+        await expect(
+          token.connect(client1).setWhitelist(client2.address),
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+        await expect(token.connect(treasuryFund).setWhitelist(client2.address))
+          .to.not.be.reverted;
+      });
+
+      it("Should Can't whitelist address if already whitelisted", async function () {
+        await token.connect(treasuryFund).setWhitelist(client3.address);
+        // so if we set same value again, its will revert
+        await expect(
+          token.connect(treasuryFund).setWhitelist(client3.address),
+        ).to.be.revertedWith("Already Whitelisted");
+      });
+
+      it("Should Can't whitelist Zero address", async function () {
+        await expect(
+          token.connect(treasuryFund).setWhitelist(zeroAddress),
+        ).to.be.revertedWith("Address Zero Not Accepted");
+      });
+
+      it("Should set correct value and emit event LogWhitelistAdded", async function () {
+        expect(await token.connect(treasuryFund).setWhitelist(client4.address))
+          .to.emit(token, "LogWhitelistAdded")
+          .withArgs(client4.address);
+
+        const isExempt = await token.checkFeeExempt(client4.address);
+        expect(isExempt).to.eq(true);
+      });
+    });
+
+    describe("removeWhitelist Function Test", function () {
+      before(async function () {
+        // Deploy contract
+        const Token = await ethers.getContractFactory("NjordContract");
+        token = await Token.deploy(
+          pancakeRouterContract.address,
+          autoLiquidityFund.address,
+          treasuryFund.address,
+          njordRiskFreeFund.address,
+          supplyControl.address,
+        );
+        await token.deployed();
+        // we whitelisted client1, client2 first, so can be removed later
+        await token.connect(treasuryFund).setWhitelist(client1.address);
+        await token.connect(treasuryFund).setWhitelist(client2.address);
+      });
+
+      it("Only Onwer can call this function", async function () {
+        await expect(
+          token.connect(client1).removeWhitelist(client2.address),
+        ).to.be.revertedWith("Ownable: caller is not the owner");
+
+        await expect(
+          token.connect(treasuryFund).removeWhitelist(client1.address),
+        ).to.not.be.reverted;
+      });
+
+      it("Should Can't remove whitelist if address already non whitelisted", async function () {
+        // client 3 is non whitelisted address
+        await expect(
+          token.connect(treasuryFund).removeWhitelist(client3.address),
+        ).to.be.revertedWith("Already Not Whitelisted");
+      });
+
+      it("Should Can't whitelist Zero address", async function () {
+        await expect(
+          token.connect(treasuryFund).removeWhitelist(zeroAddress),
+        ).to.be.revertedWith("Address Zero Not Accepted");
+      });
+
+      it("Should set correct value and emit event LogWhitelistRemoved", async function () {
+        expect(
+          await token.connect(treasuryFund).removeWhitelist(client2.address),
+        )
+          .to.emit(token, "LogWhitelistRemoved")
+          .withArgs(client2.address);
+
+        const isExempt = await token.checkFeeExempt(client2.address);
+        expect(isExempt).to.eq(false);
+      });
     });
   });
 });
